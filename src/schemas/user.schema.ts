6e5df565a63a998c70c282f2a0e36bbd3976da51
AKZ <75739105+darkelfs56@users.noreply.github.com>
2025-07-21 14:43:51 +0800

    add _id to user.schema.ts

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,10 +1,12 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 
-export type UserDocument = HydratedDocument<User>;
+export type UserDocument = mongoose.HydratedDocument<User>;
 
 @Schema()
 export class User {
+  _id: mongoose.Schema.Types.ObjectId;
+
   @Prop({ unique: true, required: true })
   email: string;
 
