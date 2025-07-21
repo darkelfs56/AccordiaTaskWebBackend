af3f986d68e08d9565db79995a2761cf922a9bdd
AKZ <75739105+darkelfs56@users.noreply.github.com>
2025-07-21 14:44:15 +0800

    rename import User as UserModel in user.repository.ts file

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -6,13 +6,15 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'src/schemas/user.schema';
 import { UserDto } from './dto/user.dto';
+import { User as UserModel } from 'src/schemas/user.schema';
 
 @Injectable()
 export class UserRepository {
   private readonly logger = new Logger(UserRepository.name);
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(UserModel.name) private readonly userModel: Model<UserModel>,
+  ) {}
 
   async createUser(createUserDto: UserDto) {
     try {
